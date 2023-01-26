a873ae2a662e4feaff028e8b4ea1115c12e11b13
Kadioba <kadiobaoff@gmail.com>
2023-01-26 16:13:55 -0300

    Versao com criacao do objeto de perguntas e respostas a serem enviadas

--- javascript/script_tela3.js
+++ javascript/script_tela3.js
@@ -1,34 +1,34 @@
-let tituloCriado;
-let urlImagemCriada;
+
+//  Variaveis globais que registram as inputs do usuario
 let numeroPerguntasCriadas;
 let numeroNiveisCriados;
+let objetoPerguntaCriada = {};
+objetoPerguntaCriada.questions = [];
+objetoPerguntaCriada.levels = [];
+
+
+// Função que valida url
+const isValidUrl = urlString=> {
+    var urlPattern = new RegExp('^(https?:\\/\\/)?'+ // validate protocol
+  '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // validate domain name
+  '((\\d{1,3}\\.){3}\\d{1,3}))'+ // validate OR ip (v4) address
+  '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // validate port and path
+  '(\\?[;&a-z\\d%_.~+=-]*)?'+ // validate query string
+  '(\\#[-a-z\\d_]*)?$','i'); // validate fragment locator
+    return !!urlPattern.test(urlString);
+}
 
 function enviarInfoQuizCriado(){
-    tituloCriado = document.querySelector(".input-tiulo-criado").value;
+    objetoPerguntaCriada.title = document.querySelector(".input-tiulo-criado").value;
 
-    urlImagemCriada = document.querySelector(".input-url-imagem-criada").value;
+    objetoPerguntaCriada.image = document.querySelector(".input-url-imagem-criada").value;
 
     numeroPerguntasCriadas = document.querySelector(".input-numero-perguntas-criadas").value;
 
     numeroNiveisCriados = document.querySelector(".input-numero-niveis-criados").value;
 
-    console.log(tituloCriado);
-    console.log(urlImagemCriada);
-    console.log(numeroPerguntasCriadas);
-    console.log(numeroNiveisCriados);
-
-    const isValidUrl = urlString=> {
-        var urlPattern = new RegExp('^(https?:\\/\\/)?'+ // validate protocol
-      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // validate domain name
-      '((\\d{1,3}\\.){3}\\d{1,3}))'+ // validate OR ip (v4) address
-      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // validate port and path
-      '(\\?[;&a-z\\d%_.~+=-]*)?'+ // validate query string
-      '(\\#[-a-z\\d_]*)?$','i'); // validate fragment locator
-        return !!urlPattern.test(urlString);
-    }
-
-    const infoQuizValidos = isValidUrl(urlImagemCriada) && tituloCriado.length >= 20 && tituloCriado.length <= 65 && numeroPerguntasCriadas >= 3 &&  numeroNiveisCriados >= 2;
-    console.log(infoQuizValidos);
+    const infoQuizValidos = isValidUrl(objetoPerguntaCriada.image) && objetoPerguntaCriada.title.length >= 20 && objetoPerguntaCriada.title.length <= 65 && numeroPerguntasCriadas >= 3 &&  numeroNiveisCriados >= 2;
+    console.log(objetoPerguntaCriada);
 
     if(infoQuizValidos){
         document.querySelector(".info-quiz").classList.add("hiden");
@@ -42,7 +42,7 @@ function enviarInfoQuizCriado(){
 }
 
 function gerarInputPerguntas(){
-    const divPerguntas = document.querySelector(".perguntas");
+    const divPerguntas = document.querySelector(".aba-perguntas");
     divPerguntas.innerHTML = "";
     let templatePergunta = "";
 
@@ -54,29 +54,29 @@ function gerarInputPerguntas(){
                     <h2>Pergunta ${i}</h2>
                     <img src="./imagens/Vector.png" alt="" class="icone-pergunta hiden">
                 </div>
-                <div  class="input-perguntas">
-                    <input type="text" placeholder="Texto da pergunta">
-                    <input type="text" placeholder="Cor de fundo da pergunta">
+                <div class="input-perguntas">
+                    <input type="text" placeholder="Texto da pergunta" class="titulo-pergunta">
+                    <input type="text" placeholder="Cor de fundo da pergunta" class="cor-pergunta">
                     <h2>Resposta correta</h2>
-                    <input type="text" placeholder="Resposta correta">
-                    <input type="text" placeholder="URL da imagem">
+                    <input type="text" placeholder="Resposta correta" class="resposta-correta">
+                    <input type="text" placeholder="URL da imagem" class="url-imagem-correta">
                     <h2>Respostas incorretas</h2>
                     <ul>
-                        <li>
-                            <input type="text" placeholder="Resposta incorreta 1">
-                            <input type="text" placeholder="URL da imagem 1">
+                        <li class="resposta-incorreta">
+                            <input type="text" placeholder="Resposta incorreta 1" class="texto-resposta-incorreta">
+                            <input type="text" placeholder="URL da imagem 1" class="url-resposta-incorreta">
                         </li>
                     </ul>
                     <ul>
-                        <li>
-                            <input type="text" placeholder="Resposta incorreta 2">
-                            <input type="text" placeholder="URL da imagem 2">
+                        <li class="resposta-incorreta">
+                            <input type="text" placeholder="Resposta incorreta 2" class="texto-resposta-incorreta">
+                            <input type="text" placeholder="URL da imagem 2" class="url-resposta-incorreta">
                         </li>
                     </ul>
                     <ul>
-                        <li>
-                            <input type="text" placeholder="Resposta incorreta 3">
-                            <input type="text" placeholder="URL da imagem 3">
+                        <li class="resposta-incorreta">
+                            <input type="text" placeholder="Resposta incorreta 3" class="texto-resposta-incorreta">
+                            <input type="text" placeholder="URL da imagem 3" class="url-resposta-incorreta">
                         </li>
                     </ul>
                 </div>
@@ -89,29 +89,29 @@ function gerarInputPerguntas(){
                     <h2>Pergunta ${i}</h2>
                     <img src="./imagens/Vector.png" alt="" class="icone-pergunta">
                 </div>
-                <div  class="input-perguntas hiden">
-                    <input type="text" placeholder="Texto da pergunta">
-                    <input type="text" placeholder="Cor de fundo da pergunta">
+                <div class="input-perguntas hiden">
+                    <input type="text" placeholder="Texto da pergunta" class="titulo-pergunta">
+                    <input type="text" placeholder="Cor de fundo da pergunta" class="cor-pergunta">
                     <h2>Resposta correta</h2>
-                    <input type="text" placeholder="Resposta correta">
-                    <input type="text" placeholder="URL da imagem">
+                    <input type="text" placeholder="Resposta correta" class="resposta-correta">
+                    <input type="text" placeholder="URL da imagem" class="url-imagem-correta">
                     <h2>Respostas incorretas</h2>
                     <ul>
-                        <li>
-                            <input type="text" placeholder="Resposta incorreta 1">
-                            <input type="text" placeholder="URL da imagem 1">
+                        <li class="resposta-incorreta">
+                            <input type="text" placeholder="Resposta incorreta 1" class="texto-resposta-incorreta">
+                            <input type="text" placeholder="URL da imagem 1" class="url-resposta-incorreta">
                         </li>
                     </ul>
                     <ul>
-                        <li>
-                            <input type="text" placeholder="Resposta incorreta 2">
-                            <input type="text" placeholder="URL da imagem 2">
+                        <li class="resposta-incorreta">
+                            <input type="text" placeholder="Resposta incorreta 2" class="texto-resposta-incorreta">
+                            <input type="text" placeholder="URL da imagem 2" class="url-resposta-incorreta">
                         </li>
                     </ul>
                     <ul>
-                        <li>
-                            <input type="text" placeholder="Resposta incorreta 3">
-                            <input type="text" placeholder="URL da imagem 3">
+                        <li class="resposta-incorreta">
+                            <input type="text" placeholder="Resposta incorreta 3" class="texto-resposta-incorreta">
+                            <input type="text" placeholder="URL da imagem 3" class="url-resposta-incorreta">
                         </li>
                     </ul>
                 </div>
@@ -126,9 +126,8 @@ function gerarInputPerguntas(){
 function abrirInputPerguntas(divInputPergunta){
 
     const perguntaJaSelecionada = document.querySelector(".perguntas .selecionada");
-    console.log(perguntaJaSelecionada);
 
-    // Se ja houver uma pergunta selecionada, vai esonder ela e abrir a que foi clicada
+    // Se ja houver uma pergunta selecionada, vai esonder ela e abrir a nova selecionada
     if(perguntaJaSelecionada != null){
         perguntaJaSelecionada.querySelector(".input-perguntas").classList.add("hiden");
         perguntaJaSelecionada.classList.remove("selecionada");
@@ -139,3 +138,43 @@ function abrirInputPerguntas(divInputPergunta){
     divInputPergunta.querySelector(".input-perguntas").classList.remove("hiden");
     divInputPergunta.querySelector(".icone-pergunta").classList.add("hiden");
 }
+
+function enviarPerguntasCriadas(){
+
+    // Seleciona todos os elementos de input das perguntas
+    const elementoPerguntasCriadas = document.querySelectorAll(".input-perguntas");
+    console.log(elementoPerguntasCriadas)
+    
+    // Array que recebe os objetos de cada pergunta
+    const objetoPerguntaParaEnviar = objetoPerguntaCriada.questions;
+
+    elementoPerguntasCriadas.forEach(pergunta => {
+
+        // Objeto da pergunta que recebe titulo, cor e respostas (repostas = array de objetos)
+        const objetoPergunta = {};
+        objetoPergunta.title = pergunta.querySelector(".titulo-pergunta").value;
+        objetoPergunta.color = pergunta.querySelector(".cor-pergunta").value;
+
+        // Array que recebe objetos de resposta
+        objetoPergunta.answers = [];
+        const arrayRespostas = objetoPergunta.answers;
+
+        const respostaCorreta = {isCorrectAnswer: true};
+        respostaCorreta.text = pergunta.querySelector(".resposta-correta").value;
+        respostaCorreta.image = pergunta.querySelector(".url-imagem-correta").value;
+
+        arrayRespostas.push(respostaCorreta);
+
+        const elementosRespostasErradas = pergunta.querySelectorAll(".resposta-incorreta");
+
+        elementosRespostasErradas.forEach(ElementoRespostaErrada => {
+            const respostaErrada = {isCorrectAnswer: false};
+            respostaErrada.text = ElementoRespostaErrada.querySelector(".texto-resposta-incorreta");
+            respostaErrada.image = ElementoRespostaErrada.querySelector(".url-resposta-incorreta");
+            arrayRespostas.push(respostaErrada);
+        });
+
+        objetoPerguntaParaEnviar.push(objetoPergunta);
+    });
+    console.log(objetoPerguntaCriada);
+    }
