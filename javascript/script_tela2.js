ad5cc52ce41894de0240a544511b47277d4bb1f9
Wellington <Wellingtoncpfilho@gmail.com>
2023-01-29 07:53:14 -0300

    aprimorado autoScroll

--- javascript/script_tela2.js
+++ javascript/script_tela2.js
@@ -12,14 +12,16 @@ const tela =document.querySelector('.tela-2');
 
 
 function iniciarQuiz(id) {
-   const tela1= document.querySelector('.tela-1')
-   tela1.classList.add('hiden')
-   tela.classList.remove('hiden')
+   const tela1= document.querySelector('.tela-1');
+   tela1.classList.add('hiden');
+   tela.classList.remove('hiden');
     
 const dados = axios.get(`https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${id}`);
     dados.then(salvarDados);
     dados.catch(()=>alert('Erro ao carregar quiz. Tente novamente mais tarde'));
 }
+
+
 
 function salvarDados(dados) {
    console.log(dados.data);
@@ -28,7 +30,7 @@ function salvarDados(dados) {
    titulo = dados.data.title;
    imgBanner =dados.data.image;
    numeroDePerguntas = perguntasDoQuiz.length;
-   idQuizAtual=dados.data.id
+   idQuizAtual=dados.data.id;
    exibirNaTela();
 }
 
@@ -48,8 +50,7 @@ function selecionarAlternativa(elemento) {
     if (respondidos === numeroDePerguntas) {
      mostrarResultado();
     }else{
-         setTimeout(()=>ProximaQuestBox.scrollIntoView({behavior:"smooth"}),2000);
-         //setTimeout(()=>window.scrollBy(0,-80),2010);
+         setTimeout(()=>ProximaQuestBox.scrollIntoView({behavior:"smooth",block: "center", inline: "nearest"}),2000);
 }
 }
 
@@ -90,7 +91,7 @@ function exibirNaTela() {
     }  
     tela.innerHTML+=
     `<button onclick="reiniciarQuiz()" class="reiniciar">Reiniciar Quizz</button>
-    <p onclick="voltarParaHome()" class="voltarHome">Voltar para Home</p>`
+    <p onclick="voltarParaHome()" class="voltarHome">Voltar para Home</p>`;
 }
     
 
@@ -100,26 +101,26 @@ function mostrarResultado() {
     montarResultado();
    const resultadot2= document.querySelector('.tela-2 .resultado');
    resultadot2.classList.remove('hiden');
-   setTimeout(()=>resultadot2.scrollIntoView({behavior: "smooth"}),2000)
+   setTimeout(()=>resultadot2.scrollIntoView({behavior: "smooth"}),2000);
 }
 
 function gerarResultado() {
     percentual = (100/numeroDePerguntas*acertos).toFixed(0);
-    let controle=0
+    let controle=0;
 
     for (let i =0; i < levelDoQuiz.length; i++) {
 
         if (levelDoQuiz[i].minValue<=percentual && levelDoQuiz[i].minValue>=controle
             ) {
-            faixaDeAcerto = levelDoQuiz[i]
-            controle = levelDoQuiz[i].minValue
+            faixaDeAcerto = levelDoQuiz[i];
+            controle = levelDoQuiz[i].minValue;
         } 
     }
 }
 
 function montarResultado() {
-    document.querySelector('.tela-2 .reiniciar').remove()
-    document.querySelector('.tela-2 .voltarHome').remove()
+    document.querySelector('.tela-2 .reiniciar').remove();
+    document.querySelector('.tela-2 .voltarHome').remove();
     
     tela.innerHTML+=`<div class="resultado hiden">
     <div class="percentualDeAcerto" >
@@ -131,7 +132,7 @@ function montarResultado() {
         </div>
   </div>
  <button onclick="reiniciarQuiz()" class="reiniciar">Reiniciar Quizz</button>
- <p onclick="voltarParaHome()" class="voltarHome">Voltar para Home</p>`
+ <p onclick="voltarParaHome()" class="voltarHome">Voltar para Home</p>`;
 }
 
 function comparador() {
@@ -139,12 +140,12 @@ function comparador() {
 }
 
 function voltarParaHome() {
-    window.location.reload(true)
+    window.location.reload(true);
 }
 
 function reiniciarQuiz() {
-    acertos=0
-    respondidos=0
-    iniciarQuiz(idQuizAtual)
-    tela.scrollIntoView({behavior: "smooth"})
+    acertos=0;
+    respondidos=0;
+    iniciarQuiz(idQuizAtual);
+    tela.scrollIntoView({behavior: "smooth"});
 }
